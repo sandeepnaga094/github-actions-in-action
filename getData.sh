92aceeb267aecb385c9e2fa3fc552ab1a2fcc52d
sandeepnaga094 <sandeepnaga094@gmail.com>
2024-12-19 23:41:02 -0500

    Update getData.sh

--- getData.sh
+++ getData.sh
@@ -1,2 +1,5 @@
-
+[ -z "$url" ] && { echo "Error: url is empty"; exit 1; }
+[ -z "$post_number" ] && { echo "Error: post number is empty"; exit 1; }
+echo "url is $url"
+echo "post number is $post_number"
 curl "${url}/${post_number}"
